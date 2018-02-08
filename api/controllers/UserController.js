db5f94720040896ba9df05ef66faec08fd89f645
Manish Vashisth <ebriks.manish@gmail.com>
2018-02-08 13:20:35 +0530

    Using Fat Arrow Function

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,48 +6,91 @@
  */
 
 module.exports = {
+        // Using Error Function or Fat Arrow Function from ES 6 
+        createUser : (req, res) => {
+            let values = {
+                name : req.body.Name,
+                email : req.body.Email,
+                password : req.body.Password            
+            };
+            User.create(values, (err, records) => {
+                if(err) return res.serverError(err)
+                else return res.ok("user created")
+            });
+        },
+        retrieveUser : (req, res) => {
+            let criteria = {};
+            User.find(criteria, (err, records) => {
+                if(err) return res.serverError(err)
+                else return res.json(records)
+            });
+        },
+        updateUserByEmail : (req, res) => {
+            let findCriteria = {
+                email : req.body.Email
+            };
+            let updatedRecords = {
+                name : req.body.Name,
+                password : req.body.Password
+            };
+            User.update(findCriteria, updatedRecords, (err, records) => {
+                if(err) return res.serverError(err)
+                else return res.json(records)
+            });
+        },
+        deleteUserByEmail : (req, res) => {
+            let criteria = {
+                email : req.body.Email
+            };
+            User.destroy(criteria, (err) => {
+                if(err) return res.negotiate(err);
+                else return res.ok("user deleted")
+            });
+        }
 
-    // Removing .Exec 
-    createUser : function(req, res){
-        let values = {
-            name : req.body.Name,
-            email : req.body.Email,
-            password : req.body.Password            
-        };
-        User.create(values, function(err, records){
-            if(err) return res.serverError(err)
-            else return res.ok("user created")
-        });
-    },
-    retrieveUser : function(req, res){
-        let criteria = {};
-        User.find(criteria, function(err, records){
-            if(err) return res.serverError(err)
-            else return res.json(records)
-        });
-    },
-    updateUserByEmail : function(req, res){
-        let findCriteria = {
-            email : req.body.Email
-        };
-        let updatedRecords = {
-            name : req.body.Name,
-            password : req.body.Password
-        };
-        User.update(findCriteria, updatedRecords, function(err, records){
-            if(err) return res.serverError(err)
-            else return res.json(records)
-        });
-    },
-    deleteUserByEmail :function(req, res){
-        let criteria = {
-            email : req.body.Email
-        };
-        User.destroy(criteria, function(err){
-            if(err) return res.negotiate(err);
-            else return res.ok("user deleted")
-        });
-    }
+
+    
+        // // Removing .Exec 
+        // createUser : function(req, res){
+        //     let values = {
+        //         name : req.body.Name,
+        //         email : req.body.Email,
+        //         password : req.body.Password            
+        //     };
+        //     User.create(values, function(err, records){
+        //         if(err) return res.serverError(err)
+        //         else return res.ok("user created")
+        //     });
+        // },
+        // retrieveUser : function(req, res){
+        //     let criteria = {};
+        //     User.find(criteria, function(err, records){
+        //         if(err) return res.serverError(err)
+        //         else return res.json(records)
+        //     });
+        // },
+        // updateUserByEmail : function(req, res){
+        //     let findCriteria = {
+        //         email : req.body.Email
+        //     };
+        //     let updatedRecords = {
+        //         name : req.body.Name,
+        //         password : req.body.Password
+        //     };
+        //     User.update(findCriteria, updatedRecords, function(err, records){
+        //         if(err) return res.serverError(err)
+        //         else return res.json(records)
+        //     });
+        // },
+        // deleteUserByEmail :function(req, res){
+        //     let criteria = {
+        //         email : req.body.Email
+        //     };
+        //     User.destroy(criteria, function(err){
+        //         if(err) return res.negotiate(err);
+        //         else return res.ok("user deleted")
+        //     });
+        // }
 
     // Craeted Through Sails Official Document
     // Proper Guide
