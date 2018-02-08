371cadef765af5f3a8e17cd95190ec03044e0113
Manish Vashisth <ebriks.manish@gmail.com>
2018-02-08 12:53:42 +0530

    removed .exec method

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,20 +6,22 @@
  */
 
 module.exports = {
+
+    // Removing .Exec 
     createUser : function(req, res){
         let values = {
             name : req.body.Name,
             email : req.body.Email,
             password : req.body.Password            
         };
-        User.create(values).exec(function(err, records){
+        User.create(values, function(err, records){
             if(err) return res.serverError(err)
             else return res.ok("user created")
         });
     },
     retrieveUser : function(req, res){
         let criteria = {};
-        User.find(criteria).exec(function(err, records){
+        User.find(criteria, function(err, records){
             if(err) return res.serverError(err)
             else return res.json(records)
         });
@@ -32,7 +34,7 @@ module.exports = {
             name : req.body.Name,
             password : req.body.Password
         };
-        User.update(findCriteria, updatedRecords).exec(function(err, records){
+        User.update(findCriteria, updatedRecords, function(err, records){
             if(err) return res.serverError(err)
             else return res.json(records)
         });
@@ -41,11 +43,54 @@ module.exports = {
         let criteria = {
             email : req.body.Email
         };
-        User.destroy(criteria).exec(function(err){
+        User.destroy(criteria, function(err){
             if(err) return res.negotiate(err);
             else return res.ok("user deleted")
         });
     }
+
+    // Craeted Through Sails Official Document
+    // Proper Guide
+    // createUser : function(req, res){
+    //     let values = {
+    //         name : req.body.Name,
+    //         email : req.body.Email,
+    //         password : req.body.Password            
+    //     };
+    //     User.create(values).exec(function(err, records){
+    //         if(err) return res.serverError(err)
+    //         else return res.ok("user created")
+    //     });
+    // },
+    // retrieveUser : function(req, res){
+    //     let criteria = {};
+    //     User.find(criteria).exec(function(err, records){
+    //         if(err) return res.serverError(err)
+    //         else return res.json(records)
+    //     });
+    // },
+    // updateUserByEmail : function(req, res){
+    //     let findCriteria = {
+    //         email : req.body.Email
+    //     };
+    //     let updatedRecords = {
+    //         name : req.body.Name,
+    //         password : req.body.Password
+    //     };
+    //     User.update(findCriteria, updatedRecords).exec(function(err, records){
+    //         if(err) return res.serverError(err)
+    //         else return res.json(records)
+    //     });
+    // },
+    // deleteUserByEmail :function(req, res){
+    //     let criteria = {
+    //         email : req.body.Email
+    //     };
+    //     User.destroy(criteria).exec(function(err){
+    //         if(err) return res.negotiate(err);
+    //         else return res.ok("user deleted")
+    //     });
+    // }
 	
 };
 
